c6968ef5902d819ca0b005f62c7f09ff867b94cb
Sushi <kylieoiyingchan@gmail.com>
2023-06-22 15:23:29 -0700

    Update Home.jsx
    Changed

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,16 +5,10 @@ export default function Home() {
 
   return (
     <div>
-      <h1>MDIA 4590 - Week 04 Lab</h1>
+      <h1>MDIA 4590 - Week 07 Lab</h1>
 
-      <p>This example is meant to give you some components and processes to check against the VSC debugger:</p>
+      <p>Demonstration of git.</p>
 
-      <ul>
-        <li>First you'll need to configure the project for debugging via the launch.json file</li>
-        <li>Then you'll start the project using F5</li>
-        <li>After this you can try adding breakpoints to the RecipeApi or TodoApp components or their child components</li>
-        <li>Use those breakpoints to monitor variables and step over or into statements using F10 and F11</li>
-      </ul>
 
 
     </div>
